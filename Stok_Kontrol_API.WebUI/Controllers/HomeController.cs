77652eb513ae22aa505393c5691839dcd68980c6
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-01-17 18:50:33 +0300

    Bug clean

---
+++ Stok_Kontrol_API.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stok_Kontrol_API.WebUI.Models;
+using System.Diagnostics;
+
+namespace Stok_Kontrol_API.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
