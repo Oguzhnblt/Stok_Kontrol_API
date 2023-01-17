0559313231dd4d04b8374e1d1c9f6638647042be
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-01-18 00:45:12 +0300

    UI design in progress

---
+++ Stok_Kontrol_API.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Stok_Kontrol_API.Entities.Entities;
+using System.Text;
+
+namespace Stok_Kontrol_API.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class CategoryController : Controller
+    {
+
+        string uri = "https://localhost:7233";
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = new List<Category>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var cevap = await httpClient.GetAsync($"{uri}/api/Category/TumKategorileriGetir"))
+                {
+                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+
+                    categories = JsonConvert.DeserializeObject<List<Category>>(apiCevap);
+                }
+            }
+            return View(categories);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> KategoriAktiflestir(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var cevap = await httpClient.GetAsync($"{uri}/api/Category/KategoriAktiflestir/{id}"))
+                {
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        public async Task<IActionResult> KategoriSil(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var cevap = await httpClient.DeleteAsync($"{uri}/api/Category/KategoriSil/{id}"))
+                {
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult KategoriEkle()
+        {
+            return View(); // Sadece ekleme View'ını gösterecek.
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> KategoriEkle(Category category)
+        {
+            category.isActive = true;
+
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(category), Encoding.UTF8, "application/json");
+
+                using (var cevap = await httpClient.PostAsync($"{uri}/api/Category/KategoriEkle", content))
+                {
+                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Stok_Kontrol_API.WebUI/Program.cs
+++ Stok_Kontrol_API.WebUI/Program.cs
@@ -21,12 +21,15 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+   name: "areas",
+   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+   );
 app.MapControllerRoute(
-           name: "areas",
-           pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-         );
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+
 
 app.Run();
